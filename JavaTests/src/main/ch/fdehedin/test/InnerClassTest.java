b3ad7f687ffc3c6e43364095a3d4eddd34be00d7
Frédéric Dehédin <frederic.dehedin@belsoft.ch>
2016-08-22 15:25:25 +0200

    initial commit

---
+++ JavaTests/src/main/ch/fdehedin/test/InnerClassTest.java
@@ -0,0 +1,62 @@
+package ch.fdehedin.test;
+
+import java.util.Iterator;
+
+public class InnerClassTest {
+
+	int num;
+
+	// inner class
+	private class Inner_Demo {
+		public void print() {
+			System.out.println("This is an inner class");
+		}
+	}
+
+	// Accessing he inner class from the method within
+	void display_Inner() {
+		Inner_Demo inner = new Inner_Demo();
+		inner.print();
+	}
+
+	public void showDemo() {
+		Inner_Demo demo = new Inner_Demo();
+		demo.print();
+	}
+
+	public static void main(final String[] args) {
+		InnerClassTest test = new InnerClassTest();
+		test.showDemo();
+
+		AnonymousInner inner = new AnonymousInner() {
+
+			@Override
+			public void mymethod() {
+				System.out.println("This is an example of anonymous inner class");
+			}
+		};
+		inner.mymethod();
+	}
+	
+	public Iterator<Integer> createIntegerIterator( final int from, final int to)
+	{
+	    return new Iterator<Integer>(){
+	        int index = from;
+	        @Override
+			public Integer next()
+	        {
+	            return index++;
+	        }
+	        @Override
+			public boolean hasNext()
+	        {
+	            return index <= to;
+	        }
+	        // remove method omitted
+	    };
+	}
+} 
+
+abstract class AnonymousInner {
+	public abstract void mymethod();
+}

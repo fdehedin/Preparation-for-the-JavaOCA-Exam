d760e84cb2144a3999797ec3b3696a169e1c3b64
Frédéric Dehédin <frederic.dehedin@belsoft.ch>
2016-08-29 23:59:19 +0200

    Some tests..

--- SuperJavaTests/src/main/ch/fdehedin/test/chapter1/ClassVariables.java
+++ SuperJavaTests/src/main/ch/fdehedin/test/chapter1/ClassVariables.java
@@ -1,5 +1,33 @@
+//comment for package
 package ch.fdehedin.test.chapter1;
+
+import org.junit.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ClassVariables {
  
+	private static final Logger logger = LoggerFactory.getLogger(ClassVariables.class);
+
+	
+	static boolean b;
+	static byte b1;
+	static short s;
+	static int i;
+	static long l;
+	static char c;
+	static Object o;
+	
+	public static void main(String[] args) {
+	
+		logger.debug("boolean: "+b);
+		logger.debug("byte: "+b1);
+		logger.debug("short: "+s);
+		logger.debug("int: "+i);
+		logger.debug("long: "+l);
+		logger.debug("char: "+c);
+		logger.debug("Object: "+o);
+		}
+	
 }

5d27694f60611b5a78b2e90a19542beca4c846e6
Frédéric Dehédin <frederic.dehedin@belsoft.ch>
2016-10-06 22:28:06 +0200

    testing local variables

--- SuperJavaTests/src/main/ch/fdehedin/test/chapter1/ClassVariables.java
+++ SuperJavaTests/src/main/ch/fdehedin/test/chapter1/ClassVariables.java
@@ -7,37 +7,54 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ClassVariables {
- 
-	private int inte = 123;
-	
-	double amont = 0b101;
-	double amont2 = 0xE;
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ClassVariables.class);
-	
-	String name;
-	static boolean b;
-	static byte b1;
-	static short s;
-	static int i;
-	static long l;
-	static char c;
-	static Object o;
-	
+
+	static String name; // null
+	static boolean b;// false
+	static byte b1;// 0
+	static short s;// 0
+	static int i;// 0
+	static long l;// 0
+	static char c;// \u0000
+	static Object o;// null
+	static Integer in;// null
+
 	public static void main(String[] args) {
-	 
-		ClassVariables uio = new ClassVariables();
-		System.out.println(uio.inte); 
-		boolean b2;
-	//logger.debug("boolean: "+b2); //method variables need to be initialized..
-		System.out.println(i); 
-		logger.debug("boolean: "+b);
-		logger.debug("byte: "+b1);
-		logger.debug("short: "+s);
-		logger.debug("int: "+i);
-		logger.debug("long: "+l);
-		logger.debug("char: "+c);
-		logger.debug("Object: "+o);
-		}
-	
+		logger.debug("String: " + name);
+		logger.debug("boolean: " + b);
+		logger.debug("byte: " + b1);
+		logger.debug("short: " + s);
+		logger.debug("int: " + i);
+		logger.debug("long: " + l);
+		logger.debug("char: " + c);
+		logger.debug("Object: " + o);
+		logger.debug("Integer: " + in);
+	}
+
+	public static void testLocalVariables() {
+		String name;
+		boolean b;
+		byte b1;
+		short s;
+		int i;
+		long l;
+		char c;
+		Object o;
+		Integer in;
+
+		/*
+		 * 
+		 * the following code will not compile, since local variables need to be
+		 * initialized
+		 *
+		 * 
+		 * logger.debug("String: " + name); logger.debug("boolean: " + b);
+		 * logger.debug("byte: " + b1); logger.debug("short: " + s);
+		 * logger.debug("int: " + i); logger.debug("long: " + l);
+		 * logger.debug("char: " + c); logger.debug("Object: " + o);
+		 * logger.debug("Integer: " + in);
+		 */
+	}
+
 }

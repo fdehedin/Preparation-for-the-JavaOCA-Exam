a4522f91fbedeb3f5caf06fca16758ee4c43bcbc
Frédéric Dehédin <frederic.dehedin@belsoft.ch>
2016-10-22 19:02:52 +0200

    StringBuilder more tests..

--- SuperJavaTests/src/test/ch/fdehedin/oca/CoreJavaApis/StringBuilderTester.java
+++ SuperJavaTests/src/test/ch/fdehedin/oca/CoreJavaApis/StringBuilderTester.java
@@ -16,6 +16,9 @@ public class StringBuilderTester {
 
 	private static final Logger log = Logger.getLogger(StringPooler.class.getSimpleName());
 
+	@Rule
+	public final ExpectedException exception = ExpectedException.none();
+
 	@Test
 	public void testImmutableString() {
 		String alpha = "";
@@ -53,10 +56,63 @@ public class StringBuilderTester {
 		StringBuilder b = a.append("de");
 		b.append("f");
 		b = b.append("g").append('-').append(true);
-	
 
 		assertThat(a.toString(), equalTo("abcdefg-true"));
 		assertThat(b.toString(), equalTo("abcdefg-true"));
 		assertThat(a.hashCode(), is(b.hashCode()));
+	}
+
+	@Test
+	public void testInsert() {
+		StringBuilder a = new StringBuilder("abcdefg");
+		a.insert(2, "-");
+
+		assertThat(a.toString(), equalTo("ab-cdefg"));
+		assertThat(a.toString(), not(equalTo("abc-defg")));
+
+		// this will insert the - BEFORE the char at position 5;
+		a = new StringBuilder("012345");
+		a.insert(5, "-");
+		assertThat(a.toString(), equalTo("01234-5"));
+
+		// this will insert the - at the END;
+		a = new StringBuilder("012345");
+		a.insert(6, "-");
+		assertThat(a.toString(), equalTo("012345-"));
+
+		// will return error, since 7 is out of bounds;
+		a = new StringBuilder("012345");
+		exception.expect(StringIndexOutOfBoundsException.class);
+		a.insert(7, "-");
+		assertThat(a.toString(), equalTo("012345-"));
+
+	}
+
+	@Test
+	public void testDelete() {
+		StringBuilder a = new StringBuilder("abcdefg");
+		a.delete(2, 3);
+
+		assertThat(a.toString(), equalTo("abdefg"));
+
+		a = new StringBuilder("abcdefg");
+		a.delete(2, 4);
+
+		assertThat(a.toString(), equalTo("abefg"));
+
+		a = new StringBuilder("abcdefg");
+		a.deleteCharAt(1);
+
+		assertThat(a.toString(), equalTo("acdefg"));
+
+	}
+	@Test
+	public void testReverse() {
+		StringBuilder a = new StringBuilder("abcdefg");
+		a.reverse();
+
+		assertThat(a.toString(), equalTo("gfedcba"));
+
+
 	}
 }
